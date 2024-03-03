2025f9cf4cf0d622c824b1bd3de05198ec225632
stromae <claudio.oliveira10.0p@gmail.com>
2024-03-03 20:26:21 -0300

    create page login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { EmailValidator, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm : FormGroup
+
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    })
+  }
+
+  submitLoginForm() {
+    console.log(this.loginForm.value)
+  }
+
+}
