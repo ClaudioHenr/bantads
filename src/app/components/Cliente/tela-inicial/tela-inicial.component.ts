e9016075837fd37f2fda6c06ae6154140ec08c2d
brksaian <braianviana@hotmail.com>
2024-03-07 18:58:09 -0300

    Adição da tela inicial do cliente

---
+++ src/app/components/Cliente/tela-inicial/tela-inicial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-inicial',
+  standalone: true,
+  imports: [],
+  templateUrl: './tela-inicial.component.html',
+  styleUrl: './tela-inicial.component.css'
+})
+export class TelaInicialComponent {
+
+}
