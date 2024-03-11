dc95c2c2229eb34341f09d895ea506a7587db015
brksaian <braianviana@hotmail.com>
2024-03-11 19:26:51 -0300

    Correção do cast to date na tela inicial do cliente

--- src/app/components/Cliente/tela-inicial/tela-inicial.component.ts
+++ src/app/components/Cliente/tela-inicial/tela-inicial.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-tela-inicial',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './tela-inicial.component.html',
   styleUrl: './tela-inicial.component.css',
 })
-export class TelaInicialComponent {}
+export class TelaInicialComponent {
+  cliente = {
+    nome: 'João da Silva',
+    numeroConta: '123456789',
+    saldo: 5000.5,
+    transacoes: [
+      { data: new Date(), descricao: 'Depósito', valor: 1000.0 },
+      { data: new Date(), descricao: 'Compra', valor: -500.5 },
+      // Adicione mais transações conforme necessário
+    ],
+  };
+}
