546f6be763f4bca9c350cb0e25f52fdf615caa6f
stromae <claudio.oliveira10.0p@gmail.com>
2024-03-13 14:03:47 -0300

    adicionado rota para login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { TelaInicialComponent } from './Cliente/tela-inicial/tela-inicial.component';
 import { AutocadastroComponent } from './autenticacao/autocadastro/autocadastro.component';
+import { LoginComponent } from './autenticacao/login/login.component';
 
 export const routes: Routes = [
   {
@@ -11,4 +12,8 @@ export const routes: Routes = [
     path: 'autocadastro',
     component: AutocadastroComponent,
   },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
 ];
