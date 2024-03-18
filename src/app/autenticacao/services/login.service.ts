d5013e142f7f69603eb8eb55aa359ecfaaec60fb
Claudio <85650159+estromae@users.noreply.github.com>
2024-03-18 16:40:06 -0300

    arrumado service de login

--- src/app/autenticacao/services/login.service.ts
+++ src/app/autenticacao/services/login.service.ts
@@ -5,14 +5,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LoginService {
-  private endpointUrl = ''
+  private endpointUrl = 'http://localhost:3000/login'
+
+  // Dados de Usuário p/ autenticação: Cliente ou Gerente, Tipo (cliente/gerente/admin), login, senha
 
   constructor(private http: HttpClient) { }
 
-  sendDataToLogin(email: string, password: string) {
-    const data = {email, password}
-    console.log("Login feito")
-    //return this.http.post(this.endpointUrl, data)
+  sendDataToLogin(auth: autenticacao) {
+    console.log(auth.login, auth.senha)
+    const data = {
+      email: auth.login, 
+      senha: auth.senha
+    }
+    console.log("Contatando API...")
+    this.http.post(this.endpointUrl, data).subscribe(
+      (response) => console.log("Resposta de login recebida", response)
+    )
   }
-
 }
