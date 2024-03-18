43ab4fb9114544147e400425f4a6220fe8318539
Claudio <85650159+estromae@users.noreply.github.com>
2024-03-18 16:41:17 -0300

    arrumado service de register

--- src/app/autenticacao/services/register.service.ts
+++ src/app/autenticacao/services/register.service.ts
@@ -7,25 +7,31 @@ import { Endereco } from '../../shared/models/endereco.model';
   providedIn: 'root'
 })
 export class RegisterService {
-  private endpoint = ''
-
+  private endpoint = 'http://localhost:3000/clientes'
+  
   constructor(private http: HttpClient ) { }
+  
+  // Lidar com:
+  // MS Cliente: Criação do registro de Cliente
+  // MS Autenticação: Criação do registro de autenticação do cliente
+  // MS Conta: Criação do registro de conta do cliente
+  // MS Gerente: Consulta de gerentes e contas para decidir o gerente que assume a nova conta
 
   sendDataForRegister(client: Cliente, address: Endereco) {
     const data = {
       nome: client.nome,
       email: client.email,
       cpf: client.cpf,
-      endereco: address,
+      endereco: address.endereco,
       telefone: client.telefone,
       salario: client.salario
     }
-    this.http.post(this.endpoint, data)
+    const dadosFormatados = `Nome: ${data.nome}, Email: ${data.email}, CPF: ${data.cpf}, Endereço: ${data.endereco}, Telefone: ${data.telefone}, Salário: ${data.salario}`;
+    console.log("Enviando para gateway...", dadosFormatados);
+
+    this.http.post(this.endpoint, data).subscribe(
+      (response) => console.log("Resposta recebida", response)
+    )
   }
 
-  // sendDataForRegister(name: string, email: string, cpf: string, phone: number, password: string) {
-  //   const data = {name, email, cpf, phone, password}
-  //   console.log("Cadastro feito")
-  //   //return this.http.post(this.endpoint, data)
-  // }
 }
